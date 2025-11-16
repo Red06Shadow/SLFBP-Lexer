338ec4b9bfe2b5316c81430fe8f1793235df3e5b
Red06Shadow <alexandrosdiestegarcia4444@gmail.com>
2025-11-15 19:45:07 -0500

    Se agrega el codigo del proyecto

---
+++ include/stda/system/files/files.hpp
@@ -0,0 +1,8 @@
+#include <stda/system/files/url/url.hpp>
+#include <stda/system/files/exception/exception.hpp>
+#include <stda/system/files/fstream/fstream.hpp>
+#include <stda/system/files/directory/directory.hpp>
+
+/* @Documentacion:
+ * Este archivo tiene como unico proposito incluir todos los archivos dentro de tu proyecto, mas nada... :)
+ */
---
+++ lib/lexer/lexer.cpp
@@ -0,0 +1,57 @@
+#include <lexer/lexer.hpp>
+
+slfbp::lexer::lexer(std::initializer_list<const char *> args_symbols)
+{
+    for (auto &&i : args_symbols)
+        symbols_reference[i] = true;
+}
+
+void slfbp::lexer::add(std::string &str, slfbp::token::id type, std::vector<slfbp::token> &alloc)
+{
+    if (!str.empty())
+    {
+        alloc.push_back(slfbp::token(str.c_str(), str.size(), type));
+        str.clear();
+    }
+}
+
+void slfbp::lexer::tokenize(const std::string &str, std::vector<slfbp::token> &alloc)
+{
+    const char *cstr = str.c_str();
+    std::string word;
+    bool alfanum = true;
+    while (*cstr)
+    {
+        ///////////////////////////////////////////////////////
+        if (std::isspace(*cstr))
+            slfbp::lexer::add(word, alfanum ? slfbp::token::id::word : slfbp::token::id::symbol, alloc);
+        else if (alfanum == sstring::isalfanum(*cstr))
+        {
+            word.push_back(*cstr);
+            if (!alfanum)
+            {
+                if (!this->symbols_reference[word])
+                {
+                    word.pop_back();
+                    slfbp::lexer::add(word, slfbp::token::id::symbol, alloc);
+                    word.push_back(*cstr);
+                }
+            }
+        }
+        else
+        {
+            slfbp::lexer::add(word, alfanum ? slfbp::token::id::word : slfbp::token::id::symbol, alloc);
+            word.push_back(*cstr);
+            alfanum = !alfanum;
+        }
+        ///////////////////////////////////////////////////////
+        cstr++;
+    }
+    if (!word.empty())
+        slfbp::lexer::add(word, alfanum ? slfbp::token::id::word : slfbp::token::id::symbol, alloc);
+}
+
+void slfbp::lexer::view(const std::vector<slfbp::token> &alloc) {
+    for (auto &&i : alloc)
+        std::cout << "[\"" << i._atom << "\", \"" << (int)(*i._id) << "\"]" << std::endl;
+}
